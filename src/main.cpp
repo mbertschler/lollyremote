ec72e193b432fbceb1ed9e0d518feb8ed4fe08f3
Martin Bertschler <mbertschler@gmail.com>
2024-07-14 21:45:19 +0200

    add rotary encoder code

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,41 @@
 #include <Arduino.h>
 
+#include <ESP32Encoder.h>
+
 // include libraries that aren't needed here, but are needed for the build to succeed
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 
 #define NUM_BUTTONS 4
-#define BUTTON_RED 15   // RED button on the breadboard
-#define BUTTON_GREEN 16 // RED button on the breadboard
-#define BUTTON_BLUE 17  // RED button on the breadboard
-#define BUTTON_BOARD 0  // the built-in BOOT button on D0
-#define LED_BOARD 2     // the built-in LED pin for ESP32 on D2
+#define BUTTON_RED 15     // RED button on the breadboard
+#define BUTTON_GREEN 16   // GREEN button on the breadboard
+#define BUTTON_ENCODER 17 // encoder SW pin on the breadboard
+#define ENCODER_DT 18     // encoder DT pin on the breadboard
+#define ENCODER_CLK 19    // encoder CLK pin on the breadboard
+#define BUTTON_BOARD 0    // the built-in BOOT button on D0
+#define LED_BOARD 2       // the built-in LED pin for ESP32 on D2
 
 // button array indices
 #define RED 0
 #define GREEN 1
-#define BLUE 2
+#define ENCODER 2
 #define BOARD 3
 
-int buttonPins[NUM_BUTTONS] = {BUTTON_RED, BUTTON_GREEN, BUTTON_BLUE, BUTTON_BOARD}; // the number of the pushbutton pin
-int buttonStates[NUM_BUTTONS] = {HIGH, HIGH, HIGH, HIGH};                            // the current reading from the input pins
-int lastButtonStates[NUM_BUTTONS] = {HIGH, HIGH, HIGH, HIGH};                        // the previous reading from the input pins
-unsigned long lastDebounceTimes[NUM_BUTTONS] = {0, 0, 0, 0};                         // the last time the input pins were toggled
-String buttonNames[NUM_BUTTONS] = {"red", "green", "blue", "board"};                 // the names of the buttons
+int buttonPins[NUM_BUTTONS] = {BUTTON_RED, BUTTON_GREEN, BUTTON_ENCODER, BUTTON_BOARD}; // the number of the pushbutton pin
+int buttonStates[NUM_BUTTONS] = {HIGH, HIGH, HIGH, HIGH};                               // the current reading from the input pins
+int lastButtonStates[NUM_BUTTONS] = {HIGH, HIGH, HIGH, HIGH};                           // the previous reading from the input pins
+unsigned long lastDebounceTimes[NUM_BUTTONS] = {0, 0, 0, 0};                            // the last time the input pins were toggled
+String buttonNames[NUM_BUTTONS] = {"red", "green", "encoder", "board"};                 // the names of the buttons
 
 // the debounce time, increase if the output flickers
 unsigned long debounceDelay = 50;
 
+ESP32Encoder firstEncoder;
+long firstEncoderPosition = 0;
+
 // function declarations
 void checkButton(int buttonIndex);
+void checkEncoder();
 
 void setup()
 {
@@ -35,8 +43,11 @@ void setup()
 
     pinMode(BUTTON_RED, INPUT_PULLUP);
     pinMode(BUTTON_GREEN, INPUT_PULLUP);
-    pinMode(BUTTON_BLUE, INPUT_PULLUP);
+    pinMode(BUTTON_ENCODER, INPUT_PULLUP);
     pinMode(BUTTON_BOARD, INPUT_PULLUP);
+
+    firstEncoder.attachHalfQuad(ENCODER_DT, ENCODER_CLK);
+    firstEncoder.setCount(0);
 
     // start with the LED off
     digitalWrite(LED_BOARD, LOW);
@@ -49,8 +60,9 @@ void loop()
 {
     checkButton(RED);
     checkButton(GREEN);
-    checkButton(BLUE);
+    checkButton(ENCODER);
     checkButton(BOARD);
+    checkEncoder();
 }
 
 void checkButton(int buttonIndex)
@@ -86,4 +98,15 @@ void checkButton(int buttonIndex)
 
     // save the reading. Next time through the loop, it'll be the lastButtonState:
     lastButtonStates[buttonIndex] = reading;
+}
+
+void checkEncoder()
+{
+    long newPosition = firstEncoder.getCount() / 2;
+    if (newPosition != firstEncoderPosition)
+    {
+        firstEncoderPosition = newPosition;
+        Serial.print("encoder position changed: ");
+        Serial.println(newPosition);
+    }
 }
