017cb7aa65d74130bd6041cd490564315ba067c1
Martin Bertschler <mbertschler@gmail.com>
2024-07-18 18:25:17 +0200

    adjust to finished circuit board

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,16 @@
 
 #define SLAVE_ADDRESS 0x42
 
-#define NUM_BUTTONS 4
-#define BUTTON_RED 15     // RED button on the breadboard
-#define BUTTON_GREEN 16   // GREEN button on the breadboard
-#define BUTTON_ENCODER 17 // encoder SW pin on the breadboard
-#define ENCODER_DT 18     // encoder DT pin on the breadboard
-#define ENCODER_CLK 19    // encoder CLK pin on the breadboard
-#define BUTTON_BOARD 0    // the built-in BOOT button on D0
-#define LED_BOARD 2       // the built-in LED pin for ESP32 on D2
+#define NUM_BUTTONS 10
+// #define BUTTON_RED 15     // RED button on the breadboard
+// #define BUTTON_GREEN 16   // GREEN button on the breadboard
+// #define BUTTON_ENCODER 17 // encoder SW pin on the breadboard
+#define ENCODER_LEFT_DT 18  // encoder DT pin on the breadboard
+#define ENCODER_LEFT_CLK 5  // encoder CLK pin on the breadboard
+#define ENCODER_RIGHT_DT 16 // encoder DT pin on the breadboard
+#define ENCODER_RIGHT_CLK 4 // encoder CLK pin on the breadboard
+#define BUTTON_BOARD 0      // the built-in BOOT button on D0
+#define LED_BOARD 2         // the built-in LED pin for ESP32 on D2
 
 // button array indices
 #define RED 0
@@ -23,37 +25,74 @@
 #define ENCODER 2
 #define BOARD 3
 
-int buttonPins[NUM_BUTTONS] = {BUTTON_RED, BUTTON_GREEN, BUTTON_ENCODER, BUTTON_BOARD}; // the number of the pushbutton pin
-int buttonStates[NUM_BUTTONS] = {HIGH, HIGH, HIGH, HIGH};                               // the current reading from the input pins
-int lastButtonStates[NUM_BUTTONS] = {HIGH, HIGH, HIGH, HIGH};                           // the previous reading from the input pins
-unsigned long lastDebounceTimes[NUM_BUTTONS] = {0, 0, 0, 0};                            // the last time the input pins were toggled
-String buttonNames[NUM_BUTTONS] = {"red", "green", "encoder", "board"};                 // the names of the buttons
+int buttonPins[NUM_BUTTONS] = {
+    23,
+    33,
+    26,
+    13,
+    32,
+    25,
+    27,
+    14,
+    19,
+    17,
+}; // the number of the pushbutton pin
+int buttonStates[NUM_BUTTONS] = {
+    HIGH,
+    HIGH,
+    HIGH,
+    HIGH,
+    HIGH,
+    HIGH,
+    HIGH,
+    HIGH,
+    HIGH,
+    HIGH,
+}; // the current reading from the input pins
+int lastButtonStates[NUM_BUTTONS] = {
+    HIGH,
+    HIGH,
+    HIGH,
+    HIGH,
+    HIGH,
+    HIGH,
+    HIGH,
+    HIGH,
+    HIGH,
+    HIGH,
+}; // the previous reading from the input pins
+unsigned long lastDebounceTimes[NUM_BUTTONS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};                // the last time the input pins were toggled
+String buttonNames[NUM_BUTTONS] = {"1", "2", "3", "4", "5", "6", "7", "8", "enc 1", "enc 2"}; // the names of the buttons
 
 // the debounce time, increase if the output flickers
 unsigned long debounceDelay = 50;
 
-ESP32Encoder firstEncoder;
-long firstEncoderPosition = 0;
+ESP32Encoder leftEncoder;
+ESP32Encoder rightEncoder;
+long leftEncoderPosition = 0;
+long rightEncoderPosition = 0;
 
 // function declarations
 void checkButton(int buttonIndex);
-void checkEncoder();
+void checkEncoders();
 void receiveEvent(int howMany);
 void requestEvent();
 
-char nextTransmission = 0;
+unsigned int nextTransmission = 0;
 
 void setup()
 {
     pinMode(LED_BOARD, OUTPUT);
 
-    pinMode(BUTTON_RED, INPUT_PULLUP);
-    pinMode(BUTTON_GREEN, INPUT_PULLUP);
-    pinMode(BUTTON_ENCODER, INPUT_PULLUP);
-    pinMode(BUTTON_BOARD, INPUT_PULLUP);
+    for (int i = 0; i < NUM_BUTTONS; i++)
+    {
+        pinMode(buttonPins[i], INPUT_PULLUP);
+    }
 
-    firstEncoder.attachHalfQuad(ENCODER_DT, ENCODER_CLK);
-    firstEncoder.setCount(0);
+    leftEncoder.attachHalfQuad(ENCODER_LEFT_DT, ENCODER_LEFT_CLK);
+    leftEncoder.setCount(0);
+    rightEncoder.attachHalfQuad(ENCODER_RIGHT_DT, ENCODER_RIGHT_CLK);
+    rightEncoder.setCount(0);
 
     // start with the LED off
     digitalWrite(LED_BOARD, LOW);
@@ -68,11 +107,11 @@ void setup()
 
 void loop()
 {
-    checkButton(RED);
-    checkButton(GREEN);
-    checkButton(ENCODER);
-    checkButton(BOARD);
-    checkEncoder();
+    for (int i = 0; i < NUM_BUTTONS; i++)
+    {
+        checkButton(i);
+    }
+    checkEncoders();
 }
 
 void checkButton(int buttonIndex)
@@ -112,14 +151,22 @@ void checkButton(int buttonIndex)
     lastButtonStates[buttonIndex] = reading;
 }
 
-void checkEncoder()
+void checkEncoders()
 {
-    long newPosition = firstEncoder.getCount() / 2;
-    if (newPosition != firstEncoderPosition)
+    long pos = leftEncoder.getCount() / 2;
+    if (pos != leftEncoderPosition)
     {
-        firstEncoderPosition = newPosition;
-        Serial.print("encoder position changed: ");
-        Serial.println(newPosition);
+        leftEncoderPosition = pos;
+        Serial.print("left encoder position changed: ");
+        Serial.println(pos);
+    }
+
+    pos = rightEncoder.getCount() / 2;
+    if (pos != rightEncoderPosition)
+    {
+        rightEncoderPosition = pos;
+        Serial.print("right encoder position changed: ");
+        Serial.println(pos);
     }
 }
 
@@ -141,7 +188,14 @@ void receiveEvent(int howMany)
 void requestEvent()
 {
     Serial.println("requestEvent");
-    Wire.write(nextTransmission);
+    char b1 = nextTransmission & 0xFF;
+    char b2 = (nextTransmission >> 8) & 0xFF;
+    Wire.write(b1);
+    Wire.write(b2);
+    int left = leftEncoderPosition;
+    Wire.write(left);
+    int right = rightEncoderPosition;
+    Wire.write(right);
     nextTransmission = 0;
-    Wire.write("  hello ");
+    // Wire.write(" hello ");
 }
